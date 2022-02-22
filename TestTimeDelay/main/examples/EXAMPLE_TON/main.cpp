c9614771454da4d6fcbc1ebf02123731260fd133
nicoisadancer <l0322@schueler.ms-muc.de>
2022-02-22 10:33:01 +0100

    TON 2ausgaenge zeitverzoegert

--- TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
@@ -20,7 +20,9 @@ static const char * const TAG = "EXAMPLE_TON";
 
 
 #define BUTTON_I1 GPIO_NUM_26        // Pin 26.
+#define BUTTON_I2 GPIO_NUM_32        // Pin 32.
 #define GPIO_Q1 GPIO_NUM_19            // Pin 19.
+#define GPIO_Q2 GPIO_NUM_23
 
 
 
@@ -38,26 +40,36 @@ extern "C" void app_main(void)
        functions.)
     */
     gpio_reset_pin(GPIO_Q1);
+    gpio_reset_pin(GPIO_Q2);
     gpio_reset_pin(BUTTON_I1);
+    gpio_reset_pin(BUTTON_I2);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_Q2, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
+    gpio_set_direction(BUTTON_I2, GPIO_MODE_INPUT);
     gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
 
 
     TON TON1;
     TON1.PT = 1000;
 
+    TON TON2;
+    TON2.PT = 2000;
+
     while (true) // Endlos-Schleife
     {
         // Eingang lesen, das not wird gebraucht weil die Eingaenge bei losgelassenem Taster auf 3.3V sind, und der Taster auf GND schaltet.
         bool I1 = not gpio_get_level(BUTTON_I1);
+        bool I2 = not gpio_get_level(BUTTON_I2);
 
         // den I1 an TON1 uebergeben, und TON1 aufrufen
         TON1(I1);
+        TON2(I2);
 
         // Ausgaenge setzen
         gpio_set_level(GPIO_Q1, TON1.Q);
+        gpio_set_level(GPIO_Q2, TON2.Q);
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
